ee992e73484fb7f8932bf6dd2f2d3242d4d770ac
Difelice-e <difelice.emanuele@gmail.com>
2023-02-22 17:49:12 +0100

    created mokup for homepage

--- lib/main.dart
+++ lib/main.dart
@@ -4,122 +4,193 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({super.key, required this.home});
+  final Widget home;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: home,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return AppScaffold(home: HomePage());
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget buildHeader() {
+    return Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const <Widget>[
+            Text("Notizie dell'ultima ora",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                )),
+            Text("Top 10 storie qui per te",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
+                  color: Colors.black45,
+                )),
+          ],
+        ),
+        Expanded(child: Container()),
+        Column(
+          children: <Widget>[
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text("17 mar",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
+                  color: Colors.black45,
+                )),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget buildHeadline() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text("NOTIZIE DI PUNTA",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            )),
+        SizedBox(
+          height: 8,
+        ),
+        Text("Casa bianca, Tarrant come pelosi e Cortez - Ultima Ora",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            )),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+            "lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black45,
+            )),
+      ],
+    );
+  }
+
+  Widget buildArticle() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Column(
+          children: const <Widget>[
+            Text("Casa bianca, Tarrant come pelosi e Cortez - Ultima Ora",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                )),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+                "lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text lorem ipsum is simply dummy text",
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black45,
+                )),
+          ],
+        )),
+        SizedBox(
+          width: 16,
+        ),
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.dehaze),
+            color: Colors.black,
+          ),
+          title: Image.asset(
+            "assets/logo_ansa.png",
+            width: 150,
+          ),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: ListView.builder(
+          padding: EdgeInsets.all(16),
+          itemCount: 10 + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return buildHeader();
+            } else if (index == 1) {
+              return Padding(
+                padding: EdgeInsets.only(top: 32),
+                child: buildHeadline(),
+              );
+            } else {
+              return Padding(
+                  padding: EdgeInsets.only(top: 32), child: buildArticle());
+            }
+          },
+        ));
   }
 }
